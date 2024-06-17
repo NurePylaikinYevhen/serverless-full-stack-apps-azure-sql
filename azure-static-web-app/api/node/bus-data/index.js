0947bbff1fd9f04eac352c7ee8fb981c9742521f
NurePylaikinYevhen <166717360+NurePylaikinYevhen@users.noreply.github.com>
2024-06-17 15:33:43 +0300

    Update index.js

--- azure-static-web-app/api/node/bus-data/index.js
+++ azure-static-web-app/api/node/bus-data/index.js
@@ -1,17 +1,31 @@
-const sql = require('mssql')
+const sql = require('mssql');
 
 const AZURE_CONN_STRING = "Server=tcp:bus-server545000.database.windows.net,1433;Initial Catalog=bus-db;Persist Security Info=False;User ID=bus-admin;Password=App122331;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
-module.exports = async function (context, req) {    
-    const pool = await sql.connect(AZURE_CONN_STRING);    
+module.exports = async function (context, req) {
+    context.log('Function execution started.');
 
-    const busData = await pool.request()
-        .input("routeId", sql.Int, parseInt(req.query.rid))
-        .input("geofenceId", sql.Int, parseInt(req.query.gid))
-        .execute("web.GetMonitoredBusData");        
+    const routeId = parseInt(req.query.rid);
+    const geofenceId = parseInt(req.query.gid);
+    context.log(`Received routeId: ${routeId}, geofenceId: ${geofenceId}`);
 
-    context.res = {        
-        body: JSON.parse(busData.recordset[0]["locationData"])
-    };
-}
+    try {
+        const pool = await sql.connect(AZURE_CONN_STRING);
 
+        const busData = await pool.request()
+            .input("routeId", sql.Int, routeId)
+            .input("geofenceId", sql.Int, geofenceId)
+            .execute("web.GetMonitoredBusData");
+
+        context.res = {
+            status: 200,
+            body: JSON.parse(busData.recordset[0]["locationData"])
+        };
+    } catch (error) {
+        context.log.error('An error occurred:', error.message);
+        context.res = {
+            status: 500,
+            body: 'An error occurred. Please try again later.'
+        };
+    }
+};
